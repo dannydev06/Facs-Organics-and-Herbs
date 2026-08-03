c815bb0bcfe3a5a82075484afd678ad2f26f362b
Danny akinola <154925968+dannydev06@users.noreply.github.com>
2026-08-03 18:20:52 +0100

    Remove obsolete root files and clean project structure

--- init-db.js
+++
@@ -1,8 +0,0 @@
-// Simple script to initialize the database
-// Run this with: node init-db.js
-
-const { initializeDatabase } = require('./server/scripts/init-database');
-
-console.log('🚀 Initializing FACS Organics Database...\n');
-
-initializeDatabase();
